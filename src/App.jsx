d0d9d784ce3314be6e62840e8af92408dedb199c
AntonioBotis <botisantonio@gmail.com>
2025-11-06 01:20:37 +0200

    90%

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,90 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
-
+import { useState } from "react";
+import SelectedProject from "./components/SelectedProject.jsx";
 
 function App() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined, // undefined = ecran de start
+        projects: [],
+    });
+
+    function handleCancelAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: undefined, // înapoi la NoProjectSelected
+            };
+        });
+    }
+
+    function handleSelectProject(id) {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: id,
+            }
+        })
+    }
+
+    function handleStartAddProject() {
+        setProjectsState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null, // intrăm în modul NewProject
+            };
+        });
+    }
+
+    function handleEndAddProject(projectData) {
+        setProjectsState(prevState => {
+            const projectId = Math.random();
+            const newProject = {
+                ...projectData,
+                id: projectId,
+            };
+
+            return {
+                ...prevState,
+                selectedProjectId: undefined,        // după salvare, înapoi la ecranul de start
+                projects: [...prevState.projects, newProject],
+            };
+        });
+    }
+    function handleDeleteProject() {
+        setProjectsState((prevState) => {
+            return {
+                ...prevState,
+                selectedProjectId: undefined,
+                projects: prevState.projects.filter((project)=> project.id!==prevState.selectedProjectId)// intrăm în modul NewProject
+            };
+        });
+    }
+    const selectedProject = projectsState.projects.find(project=> project.id===projectsState.selectedProjectId );
+    let content=<SelectedProject project={selectedProject} onDelete={handleDeleteProject} />;
+
+    if (projectsState.selectedProjectId === null) {
+        content = (
+            <NewProject
+                onAdd={handleEndAddProject}
+                onCancel={handleCancelAddProject}
+            />
+        );
+    } else if (projectsState.selectedProjectId === undefined) {
+        content = (
+            <NoProjectSelected onStartAddProject={handleStartAddProject} />
+        );
+    }
+
     return (
-        <main className="h-screen my-8 flex gap-8 ">
-            <ProjectsSidebar/>
-            <NoProjectSelected/>
+        <main className="h-screen my-8 flex gap-8">
+            <ProjectsSidebar
+                onStartAddProject={handleStartAddProject}
+                projects={projectsState.projects}
+                onSelectProject={handleSelectProject}
+            />
+            {content}
         </main>
     );
 }
